22d9c172f2a4fe9e1fa82a4bf2e87d72f4b12d6e
erick <>
2024-05-17 10:31:59 -0300

    terminado repaso

--- app.js
+++ app.js
@@ -1,30 +1,16 @@
 
 
 function main() {
-    let valor = "1";
-    switch (valor) {
-        case 1:
-            console.log("Pedir turno");
-            break;
-        case 2:
-            console.log("Salir");
-            break;
-        case 3:
-            console.log("Otra cosa");
-            break;
-        default:
-            console.log("CASO POR DEFECTO");
-    }
-
-    /*  if (valor === "A") {
-         console.log("Pedir turno");
-     }else if(valor === "B"){
-         console.log("Salir");
-     }else if(valor === "C"){
-         console.log("Otra cosa");
-     }else if(valor === "D"){
-         console.log("Otra cosa 2");
-     } */
+    let opcInicio = "asdsad";
+    let cant = 0;
+    
+    do{
+        // pide al usuario un numero
+        cant++;
+        console.log("Hola");
+        opcInicio*=4;//opcInicio = opcInicio*3;
+    }while ((opcInicio%2))
+    console.log(cant);
 }
 
 main();
